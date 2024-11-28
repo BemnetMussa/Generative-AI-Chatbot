ad5f2a6138d7105088e67c62ba9891628041ecba
BemnetMussa <bemnetmussa@gmail.com>
2024-11-28 22:41:29 +0300

    seventh commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,72 @@
-import { BrowserRouter , Routes, Route } from 'react-router-dom'; // Use BrowserRouter
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import MainPage from './pages/MainPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+
+
+
+const checkAuthCookie = async () => {
+  try {
+      const response = await fetch('http://localhost:5000/api/users/protected', {
+      method: 'GET',
+      credentials: 'include', // Include cookies
+  });
+
+
+    if (response.ok) {
+
+        const data = await response.json();
+        console.log(data.userId)
+        setUserId(data.userId); // Set user ID from the response
+        setIsAuthenticated(true);
+      } else {
+
+          setIsAuthenticated(false);
+      }} catch (error) {
+          setIsAuthenticated(false);
+
+      }
+
+};
+checkAuthCookie();
+
+}, []);
+
+
+  if (isAuthenticated === null) return <p>Loading...</p>;
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/user/:id"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <MainPage />
+            </ProtectedRoute>
+          }
+        />
+        {/* Redirect based on authentication status */}
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? (
+              <Navigate to={`/user/${userId}`} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
